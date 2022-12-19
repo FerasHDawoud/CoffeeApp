70e9dc9a9f9606d705406be092685ef65a5fea10
Feras H Dawoud <62117024+FerasHDawoud@users.noreply.github.com>
2022-12-19 13:26:33 +0200

    First commit

---
+++ lib/utils/APP_COLORS.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/animation.dart';
+
+class APPCOLORS {
+  static const primaryColor = Color.fromRGBO(101, 65, 44, 1);
+  static const secondaryColor = Color.fromRGBO(255, 255, 255, 1);
+  static const primaryTextColor = Color.fromRGBO(0, 0, 0, 1);
+  static const secondaryTextColor = Color.fromRGBO(129, 129, 129, 1);
+  static const fillColor = Color.fromRGBO(252, 247, 247, 1);
+  static const aroundColor = Color.fromRGBO(229, 229, 229, 1);
+}
