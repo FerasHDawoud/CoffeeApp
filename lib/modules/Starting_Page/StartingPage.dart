629b8f17898235b59a8933b57c184b3894010a24
Feras H Dawoud <62117024+FerasHDawoud@users.noreply.github.com>
2022-12-19 14:20:26 +0200

    latte

--- lib/modules/Starting_Page/StartingPage.dart
+++ lib/modules/Starting_Page/StartingPage.dart
@@ -1,7 +1,6 @@
 // ignore_for_file:  file_names
 import 'package:coffee/utils/APP_COLORS.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class StartingPage extends StatelessWidget {
   const StartingPage({super.key});
@@ -11,13 +10,28 @@ class StartingPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: APPCOLORS.secondaryColor,
       body: Container(
+        width: double.infinity,
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/images/background.png"),
             fit: BoxFit.cover,
           ),
         ),
-        child: SvgPicture.asset("assets/icons/latte.svg"),
+        child: Column(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.15,
+            ),
+            const Text(
+              "latte",
+              style: TextStyle(
+                  fontFamily: 'Montez',
+                  color: APPCOLORS.primaryColor,
+                  fontSize: 90),
+            ),
+          ],
+        ),
       ),
     );
   }
